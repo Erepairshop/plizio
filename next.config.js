61dc208ca495cbe62261872c1944e710567eab03
Erik Borota <borota25@gmail.com>
2026-04-19 22:24:29 +0200

    next.config.js: allowedDevOrigins hard-set localhost+100.108.42.13+100.109.55.70+100.110.65.120 + typescript.ignoreBuildErrors (visszaallt 100.115.92.197-re, fix)

--- next.config.js
+++ next.config.js
@@ -2,7 +2,15 @@
 const nextConfig = {
   output: "export",
   trailingSlash: true,
-  allowedDevOrigins: ["100.115.92.197"],
+  allowedDevOrigins: [
+    "localhost",
+    "100.108.42.13",
+    "100.109.55.70",
+    "100.110.65.120",
+  ],
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
